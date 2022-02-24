e57662a8dc7b48443f511c6ad6081c2c3a278bd3
ParthDonga13 <80049125+ParthDonga13@users.noreply.github.com>
2022-02-23 19:09:17 -0500

    Touch up

--- app_public/src/components/about.jsx
+++ app_public/src/components/about.jsx
@@ -9,16 +9,14 @@ export const About = () => {
           </div>
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
-            <h3>A Little about me</h3>
-              <p>Hi, I am Parth Donga, a 24 years old Student living in Kitchener, Ontario. I am pursuing a web design and development course at Conestoga College, Canada. I am also a full-stack developer. In other words, pretty good at searching StackOverflow. I have done Computer engineering from GEC-Modasa in Gujarat, India. When I am not studying, I play guitar. I live and breathe product management every day, with a mask on.</p>
+              <h3>A Little about me</h3>
+              <p>Hi, I am Parth Donga, a 24 years old Student living in Kitchener, Ontario. I am pursuing a web design and development course at Conestoga College, Canada. I am also a full-stack developer. In other words, pretty good at searching StackOverflow. I have done Computer engineering from GEC-Modasa in Gujarat, India. When I am not studying, I play guitar. I live and breathe web development every day, with a mask on.</p>
               <h3>I build value by design.</h3>
               <div className="list-style">
                 <div className="col-xs-12">
                   <ul>
-                  <li>I strive to create elegant solutions that surprise and delight users while keeping complex technical dependencies in mind for implementation, scalability, and developer sanity.</li>
-                  <li>
-                  I have worked with small, agile teams on skunkworks projects and more significant development teams with product lifecycles spanning my college years.
-                  </li>
+                    <li>I strive to create elegant solutions that surprise and delight users while keeping complex technical dependencies in mind for implementation, scalability, and developer sanity.</li>
+                    <li> I have worked with small, agile teams on skunkworks projects and more significant development teams with product lifecycles spanning my college years.</li>
                   </ul>
                 </div>
               </div>
